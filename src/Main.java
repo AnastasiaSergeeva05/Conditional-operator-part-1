643ae15766582247daf8d200e148337b5c8191c2
AnastasiaSergeeva05 <gotsenko_nasty@mail.ru>
2022-09-10 20:10:02 +0300

    Домашнее задание. Условный оператор часть 1

---
+++ src/Main.java
@@ -0,0 +1,116 @@
+public class Main {
+    public static void main(String[] args) {
+// Домашнее задание 1
+// Задание 1
+
+        int age = 18;
+        if (age<=18){
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+// Задание 2
+        int age2 = 7;
+        if (age2>=7){
+            System.out.println("Ребенок ходит в школу");
+        }
+        int age3 = 18;
+        if (age3>=18){
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        int a = 102;
+        int b = 60;
+        int c = a - b;
+        if (b<=60) {
+            System.out.println("Сидячие места есть");
+        }
+            if (a>=102) {
+                System.out.println("Мест нет");
+            }
+                if (b>=60) {
+                    System.out.println("Сидячих мест нет");
+                }
+
+// Домашнее задание 2
+                    // Задача 1
+                    int age4 = 18;
+                    if (age4<=17){
+                        System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+                    }
+                    else {
+                        System.out.println("Возраст совершеннолетия наступил.");
+                    }
+                   // Задание 2
+
+                        int age5 = 7;
+                        if (age5<=6){
+                            System.out.println("ребенок ходит в школу");
+
+                        }
+                        else {
+                            System.out.println("ребенок ходит в садик");
+                        }
+                        // Задача 3
+                        int aa = 102;
+                        int bb = 60;
+                        int cc = aa - bb;
+                        if (bb<=bb) {
+                            System.out.println("Сидячие места есть");
+                        }
+                            else if (aa>=aa ) {
+                                System.out.println("Мест нет");
+                            }
+                                else  {
+                                    System.out.println("Сидячих мест нет");
+                                }
+
+
+// Домашнее задание 3
+
+                        //Задание 1
+                         int age6 = 2 ;
+                         int age7 = 7;
+                         int age8 = 18;
+                          if (age6>=2 && age6<=6){
+                                 System.out.println("“Если возраст человека  2 до 6 лет, то ему нужно ходить в детский сад”");
+                             }
+                          if (age7>=7 && age7<=18){
+                              System.out.println("“Если возраст человека  7 до 18 лет, то ему нужно ходить в школу”");
+                          }
+                          if (age8>=18 && age8<=24){
+                              System.out.println("“Если возраст человека  18 до 24 лет, то ему нужно ходить в университет”");
+                          }
+                          else {
+                              System.out.println("Пора ходить на работу");
+                          }
+
+                         // Задача 2
+                         int age9 = 5;
+                          int age10 = 14;
+
+                          if (age10>=5 && age10<=14){
+                              System.out.println("Ребенку можно кататься в сопровождении взрослого");
+                          }
+                          if (age10>=age10){
+                              System.out.println("Ребенку можно кататься одному");
+                          }
+                          else {
+                              System.out.println(" Ребенок не может кататься на аттракционе");
+                          }
+                            //Задача 2
+                             int one =1;
+                              int two =2;
+                              int free =3;
+                              if (one>=two && one>=free ){
+                                  System.out.println(one + "большее число");
+                              }
+                              else if(two>=one && two>=free) {
+                                  System.out.println(two + "большее число");
+                              }
+                              else {
+                                  System.out.println(free + "большее число");
+                              }
+
+      }
+    }
+
+
+
